8d7e458d97eeb0ba49d27978258e3fc26b14acb8
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-02-11 10:23:04 +0100

    Exercise 2 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,18 +1,24 @@
 package se.lexicon;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
     public static void main( String[] args ) {
         //Exercise 1
-        File file = new File("/Users/sebastianbocaciu/Documents/textfile.txt");
-        String fileContent = readText(file);
-        System.out.println(fileContent);
+        File file1 = new File("/Users/sebastianbocaciu/Documents/textfile.txt");
+        String fileContent = readText(file1);
+        //System.out.println(fileContent);
+
+        //Exercise 2
+        File url = new File("line_of_names.txt");
+            for (String string : bufferedReadText(url)) {
+                System.out.println(string);
+            }
+
 
 
     }
@@ -34,6 +40,18 @@ public class App
             return readText.toString();
         }
 
+//Ex 2
+    public static List<String> bufferedReadText(File source) {
+        List<String> strings = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                strings.add(line); }
+        } catch (IOException e) { e.printStackTrace();
+        }
+        return strings; }
 
+//Ex 3
+    public static
 
 }
